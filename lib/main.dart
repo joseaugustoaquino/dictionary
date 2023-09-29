a9ae5605cb032e795ee1ffc14780bfe72131ef91
José Augusto de Aquino <jose.aquino6@etec.sp.gov.br>
2023-09-29 09:19:40 -0300

    feat: configuration theme app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:dictionary/app/data/databases/data_base.dart';
+import 'package:dictionary/app/routes/app_routes.dart';
+import 'package:dictionary/app/routes/routes.dart';
+import 'package:dictionary/app/ui/theme/theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  /// Initial Local Storage
+  await GetStorage.init();
+
   /// Initial instace DBSet
   await DBSet.instance.database;
+  
+  /// Initial License Font Theme
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/UFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  
   runApp(const MyApp());
 }
 
@@ -27,10 +42,6 @@ class _MyAppState extends State<MyApp> {
       statusBarColor: Colors.transparent,
     ));
 
-    return main(context);
-  }
-
-  Widget main(BuildContext context) {
     return GetMaterialApp(
       title: "Dictionary",
   
@@ -42,9 +53,9 @@ class _MyAppState extends State<MyApp> {
       defaultGlobalState: true,
       enableLog: true,
 
-      home: Scaffold(
-        appBar: AppBar(),
-      ),
+      theme: ThemeSystem.theme(context),
+      initialRoute: Routes.login,
+      getPages: AppRoutes.routes,
   
       localizationsDelegates: const [
         DefaultCupertinoLocalizations.delegate,
@@ -55,12 +66,8 @@ class _MyAppState extends State<MyApp> {
         GlobalWidgetsLocalizations.delegate,
       ],
       
-      supportedLocales: const [
-        Locale('en', 'US'),
-        Locale("pt", "BR"),
-      ],
-      
-      locale: const Locale("pt", "BR"),
+      locale: const Locale('en', 'US'),
+      supportedLocales: const [Locale('en', 'US')],
     );
   }
 }
